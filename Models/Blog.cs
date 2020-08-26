f3d07910d5b29078e4f2492b285b0efbd7984b4c
Victor H <winyeah007@gmail.com>
2020-08-25 22:41:48 -0600

    add Blog.cs

---
+++ Models/Blog.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace VictorBlog.Models
+{
+    public class Blog
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int IDBlog {get; set;}
+        [Display(Name="Pls enter Blog Title:")]
+        [StringLength(20)]
+        [Required(ErrorMessage="{0} is a required field.")]
+        public string BlogTitle {get; set;}
+        [Display(Name="Pls enter Blog Content:")]
+        [StringLength(200)]
+        [Required(ErrorMessage="{0} is a required field.")]
+        public string BlogContent {get; set;}
+    }
+
+}
